54a9a78485a32745b6cbf5d5f124992a767f1646
Mohammad Amjad <themohamad.amjad@gmail.com>
2021-02-25 01:11:43 +0400

    product details page

--- cocmass/src/components/ProductDetails.jsx
+++ cocmass/src/components/ProductDetails.jsx
@@ -7,15 +7,18 @@ function ProductDetails() {
         <div className="productDetails">
             <div className="productDetails__left"><img className="product__left__image" src="https://bramleyandgage.s3.eu-west-1.amazonaws.com/wp-content/uploads/2019/10/21075042/Brunel.png" alt="A render of the Brunel Edition"  /></div>
             <div className="productDetails__right">
+                <div className="productDetails__right__container">
                 <div className="productDetails__right__title">
                     <h2>Brunel</h2>
                 </div>
                 <div className="productDetails__right__price">
                     <h2>£25.00 – £232.20</h2>
                 </div>
+                
                 <div className="productDetails__right__info">
                     <p>Exquisitely Engineered 6 O’clock Gin Brunel is an export strength expression of our classic London Dry gin.</p>
                     <p>Distilled with more juniper and 6 additional botanicals for a more complex flavour profile.</p>
+                </div>
                 </div>
                 <div className="productDetails__right__checkout">
                     <div className="productDetails__right__checkout__quantity">
