44bf368ad7f24f50e2ec80a68cb6465f98523c60
Mohammad Amjad <themohamad.amjad@gmail.com>
2021-03-23 00:45:46 +0400

    show cart items start

--- cocmass/src/components/Cart.jsx
+++ cocmass/src/components/Cart.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { db } from '../backend/firebase'
 import "../css/Cart.css"
 import BasketItem from './BasketItem'
 import OrderSummary from './OrderSummary'
 
 function Cart() {
+    const [snapshots, setSnapshots] = useState();
+    useEffect(()=> {
+        db.collection("users").doc("4sfrRMB5ROMxXDvmVdwL").collection("basket")
+         .get()
+         .then((snapshot) => {
+          setSnapshots(snapshot.docs)            
+         }
+        ) 
+        
+        ; 
+  
+    }, []);
+
+   
+    let item = ""
    
     return (
         <div className="cart">
@@ -13,10 +29,19 @@ function Cart() {
                     <h3>Shoping Basket</h3>
                 </div>
                 <div className="cart__items__item">
-                    <BasketItem />
-                    <BasketItem />
-                    <BasketItem />
-                    <BasketItem />
+             {
+                       snapshots && snapshots.map((doc)=>(
+                         
+                        db.collection("products").doc(doc.data().id).get().then(function(e){
+                               console.log(e.data().image)
+                           } ),
+                           <BasketItem />
+                           
+                           ))
+                  
+             }
+                   
+                
                 </div>
                 <div className="cart__items__continueShopping">
                 <Link to="/checkout">Continue Shopping</Link>
