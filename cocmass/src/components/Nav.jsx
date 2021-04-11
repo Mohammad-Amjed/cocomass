7aee189346370c0cc0037bce3bdf9279a95a90bf
Mohammad Amjad <themohamad.amjad@gmail.com>
2021-04-11 22:32:34 +0400

    tablet and mobile view nav bar complete

--- cocmass/src/components/Nav.jsx
+++ cocmass/src/components/Nav.jsx
@@ -1,31 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import "../css/Nav.css"
 
 function Nav() {
+    const [ToggleHeight, setToggleHeight] = useState()
+
+    const toggle = ()=>{
+        if (ToggleHeight === "toggle"){
+            setToggleHeight(undefined)
+            console.log(ToggleHeight)
+        }else{
+            setToggleHeight("toggle")
+            console.log(ToggleHeight)
+        }
+    }
     return (
         <div className="nav">
         <nav  className="nav__navContent">
             <div className="nav__navContent__logo">
                 <Link  to="/"><h1>cocomass</h1></Link>
             </div>
+            <div className={"nav__navContent__links__container " + ToggleHeight}>
             <div className="nav__navContent__links">
-                <ul id="nm-main-menu-ul" className="nm-menu">
-                    <li id="menu-item-11908" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-11908">our story</li>
-                    <li id="menu-item-11908" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-11908">Tours & experiences</li>
-                    <li id="menu-item-11908" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-11908">Coctails</li>
-                    <li id="menu-item-11908" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-11908">Shop</li>
-                    <li id="menu-item-11908" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-11908">News</li>
-                    <li id="menu-item-11908" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-11908">Contact</li>
+                <ul  className="nm-menu">
+                    <li id="menu-item-11908" className="listItem"><span>our story</span></li>
+                    <li id="menu-item-11908" className="listItem"><span>Tours &</span> experiences</li>
+                    <li id="menu-item-11908" className="listItem">Coctails</li>
+                    <li id="menu-item-11908" className="listItem">Shop</li>
+                    <li id="menu-item-11908" className="listItem">News</li>
+              
 
                 </ul>
             </div>
             <div className="nav__navContent__userInfo">
                 <ul id="nm-main-menu-ul" className="nm-menu">
-                    <li id="menu-item-11908" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-11908">Log in</li>
-                    <li id="menu-item-11908" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-11908">Basket 0</li>
+                    <li id="menu-item-11908" className="logIn">Log in</li>
                     </ul>
                     </div>
+ 
+                    </div>
+                    <div className="nav__navContent__userInfo">
+                    <ul >
+                    <li id="menu-item-11908" className="basket"><span>Basket</span> 0</li>
+                    </ul>
+                   
+                  </div>
+                  <div className="nav__navContent__burger" onClick={toggle}>
+                      <span className="nav__navContent__burger__line"></span>
+                      <span className="nav__navContent__burger__line"></span>
+                      <span className="nav__navContent__burger__line"></span>
+                  </div>
          </nav>
         </div>
     )
